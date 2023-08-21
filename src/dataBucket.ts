e687f8ea998237bb0e78ea0206c8ae2590a3a2e8
Isaac <isaachkdeveloper@gmail.com>
2023-08-21 17:01:24 +0800

    feat: build data.json

--- src/dataBucket.ts
+++ src/dataBucket.ts
@@ -1,11 +1,21 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as aws from '@pulumi/aws';
 import * as mime from 'mime';
+import * as fs from 'fs';
 
 export const initDataBucket = async (params: { name: string }) => {
   const dataBucket = new aws.s3.Bucket(params.name, {
     forceDestroy: true,
   });
+
+  let filePath = './content/data.json';
+
+  try {
+    fs.readFileSync('./data.json');
+    filePath = './data.json';
+  } catch {
+    console.log('The file is not exist');
+  }
 
   new aws.s3.BucketObject(
     `${params.name}-data.json`,
@@ -13,8 +23,8 @@ export const initDataBucket = async (params: { name: string }) => {
       key: 'data.json',
 
       bucket: dataBucket,
-      contentType: mime.getType('./content/data.json') || undefined,
-      source: new pulumi.asset.FileAsset('./content/data.json'),
+      contentType: mime.getType(filePath) || undefined,
+      source: new pulumi.asset.FileAsset(filePath),
     },
     {
       parent: dataBucket,
